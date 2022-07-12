909f848feabf9a653d361d351da352d0c8a51135
Leo-Zubiri <leo.zubiri@outlook.com>
2022-07-11 21:37:05 -0500

    update

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,8 +1,17 @@
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
+
+//Reducer
+import { deleteTask } from '../features/tasks/taskSlice';
 
 const TaskList = () => {
 
     const tasks = useSelector(state => state.tasks);
+    const dispatch = useDispatch();
+
+    const handleDelete = (id) => { 
+        console.log(id);
+        dispatch(deleteTask(id));
+    }
 
   return (
     <div>
@@ -11,6 +20,7 @@ const TaskList = () => {
                 return <div key={task.id}>
                     <h3>{task.title}</h3>
                     <p>{task.description}</p>
+                    <button onClick={()=>handleDelete(task.id)}>Delete</button>
                 </div>
             })
         }
